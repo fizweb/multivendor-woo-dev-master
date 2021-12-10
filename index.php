0ee5c335222150b2ebc06eefe55a74dc5d695a18
fizweb <mostafizdmr@gmail.com>
2021-12-10 07:25:42 +0600

    Initialized

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="content-block">
+    <div class="elementor-section elementor-section-boxed">
+        <div class="elementor-container">
+            <div class="internal-content-wrap">
+                <?php get_template_part('post-excerpt'); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
